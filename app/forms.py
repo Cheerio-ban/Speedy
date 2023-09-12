ff123050da0e61d9bf1f17243d4b8e335083f55f
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2023-09-12 12:13:10 +0100

    Implemented signup page and made it responsive

--- app/forms.py
+++ app/forms.py
@@ -12,3 +12,16 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    """
+    This class represents the Sign up web form
+    It gets inputs from the user using wtforms fields.
+    (StringField, PasswordField, SubmitField)
+    """
+    email = StringField('Email address', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app import app
 
 
@@ -11,3 +11,8 @@ def landing():
 def login():
     form = LoginForm()
     return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('registration.html', form=form)
