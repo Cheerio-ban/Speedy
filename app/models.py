23669c61ce11a629ba30443395d3b777b25299df
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2023-09-12 19:11:48 +0100

    Implemented Database Models

---
+++ app/models.py
@@ -0,0 +1,38 @@
+from app import db
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(), index=True, unique=True)
+  password_hash = db.Column(db.String(128))
+
+  def __repr__(self):
+    return '<User {}>'.format(self.username)
+
+
+class Customer(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  first_name = db.Column(db.String(140))
+  last_name = db.Column(db.String(140))
+  email = db.Column(db.String(120), index=True, unique=True)
+  phone_number = db.Column(db.Integer, primary_key=True)
+  address = db.Column(db.String()) #Consider the feasibility of making this a table of its own.
+  dob = db.Column(db.DateTime)
+  date_created= db.Column(db.DateTime)
+  accounts = db.relationship('Account', backref='customer', lazy='dynamic')
+
+
+class Accounts(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  cus_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
+  acc_type = db.Column(db.String)
+  balance = db.Column(db.Integer)
+  date_created = db.Column(db.DateTime)
+  transactions = db.relationship('transaction', backref='account', lazy='dynamic')
+
+
+class Transaction(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  acc_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+  transaction_type = db.Column(db.String)
+  amount = db.Column(db.Integer)
+  timestamp = db.Column(db.DateTime)
--- config.py
+++ config.py
@@ -2,3 +2,6 @@ import os
 
 class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or '52770cc66594d5923cb2807582ea9277'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')\
+  or 'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
