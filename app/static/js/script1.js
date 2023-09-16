920eca85c4b8b12a1995665025539320f0fefe4b
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2023-09-16 19:53:37 +0100

    Fixed Javascript errors

--- app/static/js/script1.js
+++ app/static/js/script1.js
@@ -3,21 +3,30 @@
 
 /* Create account*/
 const create = document.getElementById('create')
+const firstname = document.getElementById('firstname')
 const cancel = document.querySelector('#cancel')
 const submit = document.querySelector('#submit')
 const pop_up = document.querySelector('#pop')
 const cancel_pop = document.querySelector('#message_cancel')
-const button_message = document.querySelector('#pop button:lastchild')
+const button_message = document.querySelector('#pop button:last-child')
 const p = document.getElementsByTagName('p')
+
+create.addEventListener('click', () => {
+    console.log('created');
+})
+
+firstname.addEventListener('click', () => {
+    console.log('clicked')
+})
 
 cancel.addEventListener('click', ()=>{
 
 })
 
-document.getElementById('create').addEventListener('click', ()=>{
-    p.style.color = 'blue';
-})
+// document.getElementById('create').addEventListener('click', ()=>{
+//     p.style.color = 'blue';
+// })
 
-document.getElementsByClassName('input').addEventListener('focus', ()=>{
-    p.style.color = 'blue';
-})
+// document.getElementsByClassName('input').addEventListener('focus', ()=>{
+//     p.style.color = 'blue';
+// })
